2e2d28a783e3b2bd5afe882de4341e66bff94f94
AkshatBhat <akshatsrbhat@gmail.com>
2020-08-04 14:03:33 +0530

    Commit

--- Django_Blog/urls.py
+++ Django_Blog/urls.py
@@ -19,9 +19,11 @@ from django.contrib.auth import views as auth_views
 from users import views as users_views
 from django.conf import settings
 from django.conf.urls.static import static
+import os
 
+admin_url = os.environ['DADMIN']
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path(f'{admin_url}/', admin.site.urls),
     path('register/',users_views.register,name='register'),
     path('profile/',users_views.profile,name='profile'),
     path('login/',auth_views.LoginView.as_view(template_name='users/login.html'),name='login'),
